5107e04cbfe356553d50706b88c3a385e158d9e8
Suman Regmi <limitlessplaying@gmail.com>
2025-06-01 10:10:20 +0545

    Redesigned login and register

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from main import settings
 from .views import *
 
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
     path('login', loginPage),
     path('register', registerPage),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
